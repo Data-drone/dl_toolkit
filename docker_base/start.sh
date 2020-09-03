d4d155f5ca898508411ade2d51d943f0ca3d3c67
Data-drone <bpl.law@gmail.com>
2020-09-03 23:48:10 +1000

    updated images and wrote a build everything script with tags

--- docker_base/start.sh
+++ docker_base/start.sh
@@ -6,34 +6,34 @@ set -e
 
 # Exec the specified command or fall back on bash
 if [ $# -eq 0 ]; then
-    cmd=bash
+    cmd=( "bash" )
 else
-    cmd=$*
+    cmd=( "$@" )
 fi
 
 run-hooks () {
     # Source scripts or run executable files in a directory
     if [[ ! -d "$1" ]] ; then
-	return
+        return
     fi
     echo "$0: running hooks in $1"
-    for f in "$1"/*; do
-	case "$f" in
-	    *.sh)
-		echo "$0: running $f"
-		source "$f"
-		;;
-	    *)
-		if [[ -x "$f" ]] ; then
-		    echo "$0: running $f"
-		    "$f"
-		else
-		    echo "$0: ignoring $f"
-		fi
-		;;
-	esac
-    echo "$0: done running hooks in $1"
+    for f in "$1/"*; do
+        case "$f" in
+            *.sh)
+                echo "$0: running $f"
+                source "$f"
+                ;;
+            *)
+                if [[ -x "$f" ]] ; then
+                    echo "$0: running $f"
+                    "$f"
+                else
+                    echo "$0: ignoring $f"
+                fi
+                ;;
+        esac
     done
+    echo "$0: done running hooks in $1"
 }
 
 run-hooks /usr/local/bin/start-notebook.d
@@ -50,11 +50,12 @@ if [ $(id -u) == 0 ] ; then
     # Handle case where provisioned storage does not have the correct permissions by default
     # Ex: default NFS/EFS (no auto-uid/gid)
     if [[ "$CHOWN_HOME" == "1" || "$CHOWN_HOME" == 'yes' ]]; then
-        echo "Changing ownership of /home/$NB_USER to $NB_UID:$NB_GID"
+        echo "Changing ownership of /home/$NB_USER to $NB_UID:$NB_GID with options '${CHOWN_HOME_OPTS}'"
         chown $CHOWN_HOME_OPTS $NB_UID:$NB_GID /home/$NB_USER
     fi
     if [ ! -z "$CHOWN_EXTRA" ]; then
         for extra_dir in $(echo $CHOWN_EXTRA | tr ',' ' '); do
+            echo "Changing ownership of ${extra_dir} to $NB_UID:$NB_GID with options '${CHOWN_EXTRA_OPTS}'"
             chown $CHOWN_EXTRA_OPTS $NB_UID:$NB_GID $extra_dir
         done
     fi
@@ -65,7 +66,7 @@ if [ $(id -u) == 0 ] ; then
         # (it could be mounted, and we shouldn't create it if it already exists)
         if [[ ! -e "/home/$NB_USER" ]]; then
             echo "Relocating home dir to /home/$NB_USER"
-            mv /home/jovyan "/home/$NB_USER"
+            mv /home/jovyan "/home/$NB_USER" || ln -s /home/jovyan "/home/$NB_USER"
         fi
         # if workdir is in /home/jovyan, cd to /home/$NB_USER
         if [[ "$PWD/" == "/home/jovyan/"* ]]; then
@@ -75,18 +76,14 @@ if [ $(id -u) == 0 ] ; then
         fi
     fi
 
-    # Change UID of NB_USER to NB_UID if it does not match
-    if [ "$NB_UID" != $(id -u $NB_USER) ] ; then
-        echo "Set $NB_USER UID to: $NB_UID"
-        usermod -u $NB_UID $NB_USER
-    fi
-
-    # Set NB_USER primary gid to NB_GID (after making the group).  Set
-    # supplementary gids to NB_GID and 100.
-    if [ "$NB_GID" != $(id -g $NB_USER) ] ; then
-        echo "Add $NB_USER to group: $NB_GID"
-        groupadd -g $NB_GID -o ${NB_GROUP:-${NB_USER}}
-        usermod -g $NB_GID -a -G $NB_GID,100 $NB_USER
+    # Change UID:GID of NB_USER to NB_UID:NB_GID if it does not match
+    if [ "$NB_UID" != $(id -u $NB_USER) ] || [ "$NB_GID" != $(id -g $NB_USER) ]; then
+        echo "Set user $NB_USER UID:GID to: $NB_UID:$NB_GID"
+        if [ "$NB_GID" != $(id -g $NB_USER) ]; then
+            groupadd -f -g $NB_GID -o ${NB_GROUP:-${NB_USER}}
+        fi
+        userdel $NB_USER
+        useradd --home /home/$NB_USER -u $NB_UID -g $NB_GID -G 100 -l $NB_USER
     fi
 
     # Enable sudo if requested
@@ -96,20 +93,20 @@ if [ $(id -u) == 0 ] ; then
     fi
 
     # Add $CONDA_DIR/bin to sudo secure_path
-    sed -r "s#Defaults\s+secure_path=\"([^\"]+)\"#Defaults secure_path=\"\1:$CONDA_DIR/bin\"#" /etc/sudoers | grep secure_path > /etc/sudoers.d/path
+    sed -r "s#Defaults\s+secure_path\s*=\s*\"?([^\"]+)\"?#Defaults secure_path=\"\1:$CONDA_DIR/bin\"#" /etc/sudoers | grep secure_path > /etc/sudoers.d/path
 
     # Exec the command as NB_USER with the PATH and the rest of
     # the environment preserved
     run-hooks /usr/local/bin/before-notebook.d
-    echo "Executing the command: $cmd"
-    exec sudo -E -H -u $NB_USER PATH=$PATH XDG_CACHE_HOME=/home/$NB_USER/.cache PYTHONPATH=$PYTHONPATH $cmd
+    echo "Executing the command: ${cmd[@]}"
+    exec sudo -E -H -u $NB_USER PATH=$PATH XDG_CACHE_HOME=/home/$NB_USER/.cache PYTHONPATH=${PYTHONPATH:-} "${cmd[@]}"
 else
-    if [[ "$NB_UID" == "$(id -u jovyan)" && "$NB_GID" == "$(id -g jovyan)" ]]; then
+    if [[ "$NB_UID" == "$(id -u jovyan 2>/dev/null)" && "$NB_GID" == "$(id -g jovyan 2>/dev/null)" ]]; then
         # User is not attempting to override user/group via environment
         # variables, but they could still have overridden the uid/gid that
         # container runs as. Check that the user has an entry in the passwd
         # file and if not add an entry.
-        whoami &> /dev/null || STATUS=$? && true
+        STATUS=0 && whoami &> /dev/null || STATUS=$? && true
         if [[ "$STATUS" != "0" ]]; then
             if [[ -w /etc/passwd ]]; then
                 echo "Adding passwd file entry for $(id -u)"
@@ -145,6 +142,6 @@ else
 
     # Execute the command
     run-hooks /usr/local/bin/before-notebook.d
-    echo "Executing the command: $cmd"
-    exec $cmd
+    echo "Executing the command: ${cmd[@]}"
+    exec "${cmd[@]}"
 fi
--- docker_pytorch/README.md
+++ docker_pytorch/README.md
@@ -7,7 +7,7 @@ run with for older containers
 `docker run -d -it -p 8888:8888 -p 6006:6006 -p 8554:8554 -v /home/brian/Workspace:/home/jovyan/work --ipc=host --device=/dev/video0:/dev/video0 --device=/dev/video1:/dev/video1 datadrone/deeplearn_pytorch:cuda10.0_opencv4.0.0_tf`
 
 new instructions for latest docker:
-`docker run --gpus all -d -it -p 8989:8888 -e JUPYTER_ENABLE_LAB=yes -v /home/brian/Workspace:/home/jovyan/work --ipc=host -v /media/brian/extra_1:/home/jovyan/work/external_data datadrone/deeplearn_pytorch:latest`
+`docker run --gpus all -d -it -p 8989:8888 -e JUPYTER_ENABLE_LAB=yes -v /home/brian/Workspace:/home/jovyan/work --ipc=host -v /media/brian/extra_14:/home/jovyan/work/external_data datadrone/deeplearn_pytorch:latest`
 
 add cameras:
 `docker run --privileged --device=/dev/video0:/dev/video0 --gpus all -d -it -p 8989:8888 -e JUPYTER_ENABLE_LAB=yes -v /home/brian/Workspace:/home/jovyan/work --ipc=host -v /media/brian/extra_1:/home/jovyan/work/external_data datadrone/deeplearn_pytorch:latest`
