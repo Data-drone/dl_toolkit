d46b02f41a4b73eeb57b509bfa968a9a98c98e52
Data-drone <bpl.law@gmail.com>
2020-09-09 00:16:33 +1000

    got mxnet tagging and pushing working

--- deeplearn_mxnet/push/push_script.sh
+++ deeplearn_mxnet/push/push_script.sh
@@ -5,7 +5,7 @@ IMAGE_NAME=$1
 BASE_NAME=$2
 
 CUDA_VERSION_TAG="cuda-$(docker run --rm -a STDOUT ${IMAGE_NAME} cat /usr/local/cuda/version.txt | awk '{print $NF}'  | tr -d '\r')"
-MXNET_TAG="mxnet-$(docker run --rm -a STDOUT ${IMAGE_NAME} python -c "import mxnet; print(mxnet.__version__)" | tr -d '\r')"
+MXNET_TAG="mxnet-$(docker run --gpus all --rm -a STDOUT ${IMAGE_NAME} python -c "import mxnet; print(mxnet.__version__)" | tr -d '\r')"
 
 docker push "$IMAGE_NAME"
 docker push "$BASE_NAME:${MXNET_TAG%%\r}"
