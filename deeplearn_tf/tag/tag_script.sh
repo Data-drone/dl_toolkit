860da1f3ace69947cd2d763388b47d444b6dace7
Data-drone <bpl.law@gmail.com>
2020-09-09 18:22:15 +1000

    updated tagging and pushing scripts

---
+++ deeplearn_tf/tag/tag_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+IMAGE_NAME=$1
+BASE_NAME=$2
+
+CUDA_VERSION_TAG="cuda-$(docker run --rm -a STDOUT ${IMAGE_NAME} cat /usr/local/cuda/version.txt | awk '{print $NF}'  | tr -d '\r')"
+docker tag $IMAGE_NAME "$BASE_NAME:${CUDA_VERSION_TAG%%\r}"
+TENSORFLOW_TAG="tensorflow-$(docker run --rm -a STDOUT ${IMAGE_NAME} python -c "import tensorflow; print(tensorflow.__version__)" | tr -d '\r')"
+docker tag $IMAGE_NAME "$BASE_NAME:${TENSORFLOW_TAG%%\r}"
